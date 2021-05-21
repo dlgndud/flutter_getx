bacd8389e782fd4ac98413690fb59876b2d3bda8
dlgndud <33320066+dlgndud@users.noreply.github.com>
2021-05-22 00:26:45 +0900

    aaa

--- lib/src/controller/pixa_controller.dart
+++ lib/src/controller/pixa_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_getx/src/models/pixa_model.dart';
 import 'package:flutter_getx/src/repoistory/pixa_list.dart';
 import 'package:get/get.dart';
@@ -5,27 +6,47 @@ import 'package:get/get.dart';
 class PixaController extends GetxController {
   static PixaController get to => Get.find();
 
-  Rx<PixaModel> pixalist = PixaModel().obs;
+  Rx<PixaModel> pixalist = PixaModel(hits: []).obs;
+  RxInt _page = 1.obs;
+
+  ScrollController scrollController = ScrollController();
 
   @override
   void onInit() {
     _pixaLoad();
+    _scrollEVent();
     super.onInit();
   }
 
+  _scrollEVent() {
+    scrollController.addListener(() {
+      if (scrollController.position.pixels ==
+          scrollController.position.maxScrollExtent) {
+        //print('reload list!!!');
+        _page++;
+        _pixaLoad();
+      }
+    });
+  }
+
   void _pixaLoad() async {
+    print('Reload ${_page.value}');
     // way 1
     //PixaModel pixamodel = await PixaRepository().loadPixaImages();
 
+    // way 2
     //PixaModel pixamodel = await Get.find<PixaRepository>().loadPixaImages();  // not working
 
+    // way 3
     PixaModel pixamodel =
-        await PixaRepository.to.loadPixaImages(); // not working
+        await PixaRepository.to.loadPixaImages(_page.value); // not working
 
     if (pixamodel != null &&
         pixamodel.hits != null &&
         pixamodel.hits.length > 0) {
-      pixalist(pixamodel);
+      pixalist.update((val) {
+        val.hits.addAll(pixamodel.hits);
+      });
     }
   }
 }
--- lib/src/pages/pixa.dart
+++ lib/src/pages/pixa.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_getx/src/controller/pixa_controller.dart';
 import 'package:flutter_getx/src/models/pixa_model.dart';
@@ -31,6 +32,7 @@ class _PixaPageState extends State<PixaPage> {
     return Scaffold(
         body: Obx(
       () => CustomScrollView(
+        controller: pixaController.scrollController,
         slivers: [
           SliverAppBar(
             title: Text("Pixabay API"),
@@ -42,17 +44,25 @@ class _PixaPageState extends State<PixaPage> {
             print(hit.previewUrl);
             return Stack(children: [
               GestureDetector(
-                onTap: () {
-                  print(idx);
-                },
-                child: Container(
+                  onTap: () {
+                    print(idx);
+                  },
+                  child: Container(
                     width: _size.width,
-                    child: FadeInImage.assetNetwork(
-                      placeholder: 'assets/images/loading-buffering.gif',
-                      image: hit.previewUrl,
+                    // child: FadeInImage.assetNetwork(
+                    //   placeholder: 'assets/images/loading-buffering.gif',
+                    //   image: hit.previewUrl,
+                    //   fit: BoxFit.fitWidth,
+                    // )),
+                    child: CachedNetworkImage(
+                      imageUrl: hit.previewUrl,
+                      placeholder: (context, url) => Container(
+                        child: Center(child: CircularProgressIndicator()),
+                        height: 30,
+                      ),
                       fit: BoxFit.fitWidth,
-                    )),
-              ),
+                    ),
+                  )),
               Text(
                 hit.user,
                 style: TextStyle(fontSize: 40, color: Colors.amber),
